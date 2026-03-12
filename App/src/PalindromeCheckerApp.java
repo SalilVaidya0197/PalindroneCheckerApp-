d3e4c2b9184773f718ec7173c48814724a17853d
SalilVaidya0197 <vaidyasalil7@gmail.com>
2026-03-12 09:56:33 +0530

    uc5

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,72 +1,37 @@
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
-
-    public static boolean reverseMethod(String str) {
-        String reversed = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed = reversed + str.charAt(i);
-        }
-
-        return str.equals(reversed);
-    }
-
-    public static boolean stackMethod(String str) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : str.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : str.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean twoPointerMethod(String str) {
-        int start = 0;
-        int end = str.length() - 1;
-
-        while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-
-        return true;
-    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        long startTime, endTime;
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
 
-        startTime = System.nanoTime();
-        reverseMethod(input);
-        endTime = System.nanoTime();
-        System.out.println("Reverse Method Time: " + (endTime - startTime) + " ns");
+        boolean isPalindrome = true;
 
-        startTime = System.nanoTime();
-        stackMethod(input);
-        endTime = System.nanoTime();
-        System.out.println("Stack Method Time: " + (endTime - startTime) + " ns");
+        // Pop and compare
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
-        startTime = System.nanoTime();
-        twoPointerMethod(input);
-        endTime = System.nanoTime();
-        System.out.println("Two Pointer Method Time: " + (endTime - startTime) + " ns");
+        // Print result
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
 
         sc.close();
     }
