0f04ff8f9b297b12cdaa301936f029b8e3605e3a
SalilVaidya0197 <vaidyasalil7@gmail.com>
2026-03-12 09:32:58 +0530

    uc10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -9,14 +9,15 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        char[] chars = input.toCharArray();
+        // Normalize string (remove spaces and convert to lowercase)
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
 
         int start = 0;
-        int end = chars.length - 1;
+        int end = normalized.length() - 1;
         boolean isPalindrome = true;
 
         while (start < end) {
-            if (chars[start] != chars[end]) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
                 isPalindrome = false;
                 break;
             }
@@ -32,38 +33,5 @@ public class PalindromeCheckerApp {
 
         sc.close();
     }
-}import java.util.Scanner;
-
-public class PalindromeCheckerApp {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-
-        char[] chars = input.toCharArray();
-
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        if (isPalindrome) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not a Palindrome");
-        }
-
-        sc.close();
-    }
+}
 }
